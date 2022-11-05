45734a72d0afe5142318cf0cbcf3add9769f429c
Gabriel Ayres <Gabriel.ar.fort@gmail.com>
2022-11-04 22:13:53 -0300

    Ao clicar em um card de pokemon, é impresso no console "Achei um pokemon". Feature serve de base para o surgimento de popups ao clicar em um pokemon

--- js/main.js
+++ js/main.js
@@ -1,80 +1,100 @@
-var pokemons = []
-var pokemonsFiltrados = []
-var pokemonsCarregados = []
-var totalCarregados = 0
+var pokemons = [];
+var pokemonsFiltrados = [];
+var pokemonsCarregados = [];
+var totalCarregados = 0;
 
-pegaListaDePokemons()
-
+pegaListaDePokemons();
 
 async function pegaListaDePokemons() {
-    try {
-        const requisicao = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=100000&offset=0")
-        const requisicaoJSON = await requisicao.json()
+  try {
+    const requisicao = await fetch(
+      'https://pokeapi.co/api/v2/pokemon/?limit=100000&offset=0',
+    );
+    const requisicaoJSON = await requisicao.json();
 
-        pokemons = requisicaoJSON.results
-        pokemonsFiltrados = pokemons
-        /* nomes = pokemons.map(pokemon => pokemon.name) */
+    pokemons = requisicaoJSON.results;
+    pokemonsFiltrados = pokemons;
+    /* nomes = pokemons.map(pokemon => pokemon.name) */
 
-        alteraTotal(pokemons.length)
+    alteraTotal(pokemons.length);
 
-        carregaNovosPokemons(totalCarregados, 9)
-    } catch (erro) {
-        console.log(erro)
-    }
+    carregaNovosPokemons(totalCarregados, 9);
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
 async function carregaNovosPokemons(inicio, quantidade) {
-    const novosPokemonsCarregados = await pegaDadosDosPokemons(pokemonsFiltrados, inicio, quantidade)
-    console.log(novosPokemonsCarregados)
-    console.log(inicio)
-    console.log(quantidade)
-    pokemonsCarregados = novosPokemonsCarregados
+  const novosPokemonsCarregados = await pegaDadosDosPokemons(
+    pokemonsFiltrados,
+    inicio,
+    quantidade,
+  );
+  console.log(novosPokemonsCarregados);
+  console.log(inicio);
+  console.log(quantidade);
+  pokemonsCarregados = novosPokemonsCarregados;
 
-    geraCardsDePokemons(pokemonsCarregados)
-    totalCarregados += 9
+  geraCardsDePokemons(pokemonsCarregados);
+  totalCarregados += 9;
 }
 
 async function pegaDadosDosPokemons(pokemons, inicio, quantidade) {
-    const pokemonsSubconjunto = pokemons.slice(inicio, inicio + quantidade)
-    console.log(pokemonsSubconjunto)
-    let requisicoes = pokemonsSubconjunto.map(pokemon => requisitaDadosDeUmPokemon(pokemon.name));
+  const pokemonsSubconjunto = pokemons.slice(inicio, inicio + quantidade);
+  console.log(pokemonsSubconjunto);
+  let requisicoes = pokemonsSubconjunto.map((pokemon) =>
+    requisitaDadosDeUmPokemon(pokemon.name),
+  );
 
-    return await Promise.all(requisicoes)
+  return await Promise.all(requisicoes);
 }
 
-
 async function requisitaDadosDeUmPokemon(numero) {
-    try {
-        const requisicao = await fetch(`https://pokeapi.co/api/v2/pokemon/${numero}`)
-        const requisicaoJSON = await requisicao.json()
-        return requisicaoJSON
-    } catch (erro) {
-        console.log(erro)
-    }
-
+  try {
+    const requisicao = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${numero}`,
+    );
+    const requisicaoJSON = await requisicao.json();
+    return requisicaoJSON;
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
 function geraCardsDePokemons(pokemons) {
-    const pokemonsList = document.querySelector(".pokemons")
+  const pokemonsList = document.querySelector('.pokemons');
 
-    pokemons.forEach(pokemon => {
-        let imgURL = ''
+  pokemons.forEach((pokemon) => {
+    let imgURL = '';
 
-        if (pokemon.sprites.other.dream_world.front_default !== null) {
-            imgURL = pokemon.sprites.other.dream_world.front_default
-        } else if (pokemon.sprites.front_default !== null) {
-            imgURL = pokemon.sprites.front_default
-        } /* else {
+    if (pokemon.sprites.other.dream_world.front_default !== null) {
+      imgURL = pokemon.sprites.other.dream_world.front_default;
+    } else if (pokemon.sprites.front_default !== null) {
+      imgURL = pokemon.sprites.front_default;
+    } /* else {
             imgURL = "pokemon.sprites.other.dream_world.official-artwork"
         } */
 
-        pokemonsList.innerHTML += `
+    pokemonsList.innerHTML += `
         <div class="pokemon">
             <div class="image"><img src="${imgURL}" alt="" class="pokemon--image"></div>
             <span>${zeroPad(pokemon.id, 3)}</span>
             <p>${pokemon.name}</p>
             <img src="images/types/${pokemon.types[0].type.name}.svg">
         </div>
-        `
-    })
+        `;
+  });
 }
+
+const pokemonsDisplay = document.querySelector('.pokemons');
+
+pokemonsDisplay.addEventListener('click', (event) => {
+  parentes = event.path;
+  parentes.map((parente) => {
+    if (parente.classList != undefined) {
+      if (parente.classList.contains('pokemon')) {
+        console.log('Achei um pokemon');
+      }
+    }
+  });
+});
